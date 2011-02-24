e93474908a63c9ed3c8fde473b6cddfb0bd5fc25
James Robertson <james@r0bertson.co.uk>
2011-02-24 23:04:09 +0000

    changed from REXML to Rexle; includes dynarex declaration within the summary

--- dir-to-xml.gemspec
+++ dir-to-xml.gemspec
@@ -1,6 +1,7 @@
 Gem::Specification.new do |s|
   s.name = 'dir-to-xml'
-  s.version = '0.1.2'
+  s.version = '0.2.6'
   s.summary = 'dir-to-xml'
   s.files = Dir['lib/**/*.rb']
+  s.add_dependency('dynarex')
 end
--- lib/dir-to-xml.rb
+++ lib/dir-to-xml.rb
@@ -2,36 +2,43 @@
 
 # file: dir-to-xml.rb
 
-require 'rexml/document'
+require 'rexle'
+require 'dynarex'
 
 class DirToXML
-  #include PrettyXML
-  include REXML
 
   attr_reader :status
 
   def initialize(path= '.')
+    super()
+    old_path = Dir.pwd
     Dir.chdir  path
 
     a = Dir.glob("*").sort
     command = a.include?('dir.xml') ? 'run' : 'new_run'      
+    
     @doc, @status = self.send command, a
+    Dir.chdir old_path #File.expand_path('~')
   end
   
   def to_xml
     @doc.to_s
-  end      
+  end
+  
+  def to_dynarex
+    Dynarex.new @doc.to_s
+  end
   
   private
       
   def new_element(name, text=nil)
-    new_node = Element.new(name)
+    new_node = Rexle::Element.new(name)
     new_node.text = text if text
     new_node
   end
 
   def new_file(name, type, ext, ctime, mtime, atime)
-    node = Element.new('file')
+    node = Rexle::Element.new('file')
     node.add_element new_element('name', name)
     node.add_element new_element('type', type)
     node.add_element new_element('ext', ext)
@@ -50,31 +57,38 @@ class DirToXML
       [x, File.extname(x), File::ftype(x), File::ctime(x), File::mtime(x), File::atime(x)]
     end
 
+    records = doc.root.element('records')
+    i = '0'
     dir_files.each do |name, ext, type, ctime, mtime, atime|       
-      doc.root.elements['records'].add_element new_file(name, type, ext, ctime, mtime, atime)
+      records.add_element new_file(name, type, ext, ctime, mtime, atime)
+      i.succ!
     end
   end
 
   def new_run(a)
 
-    summary = "<summary><title>Index of %s</title><file_path>%s</file_path></summary>" % [File.basename(Dir.pwd), File.dirname(Dir.pwd)[/\/home\/james\/heroku\/(.*)/,1]]
-    #summary = "<summary/>"
-    buffer = "<?xml version='1.0' encoding='UTF-8'?><directory>%s<records/></directory>" % summary
-    doc = Document.new(buffer)
+summary = "
+<summary>
+  <title>Index of #{File.basename(Dir.pwd)}</title>
+  <file_path>#{File.dirname(Dir.pwd)}</file_path>
+  <recordx_type>dynarex</recordx_type>
+  <schema>directory[title,file_path]/file(name, type, ext, created, last_modified, last_accessed, description, owner, group, permissions)</schema>
+</summary>"
+
+    buffer = "<directory>%s<records/></directory>" % summary
+    doc = Rexle.new(buffer)
     add_files(doc, a)
 
-    doc.root.elements['records'].add_element new_file(name='dir.xml', type=nil, ext='.xml', *[Time.now] * 3)
+    doc.element('records').add_element new_file(name='dir.xml', type=nil, ext='.xml', *[Time.now] * 3)
 
-    #File.open('dir.xml','w'){|f| f.write pretty_xml(doc)}
-    File.open('dir.xml','w'){|f| doc.write f}
+    File.open('dir.xml','w'){|f| f.write doc.xml pretty: false}
     [doc, "created"]
   end
 
   def run(a)
 
-    doc = Document.new(File.open('dir.xml','r').read)   
-    #puts 'before a)dir'
-    a_dir = XPath.match(doc.root, 'records/file/name/text()').map {|x| x.to_s}.sort
+    doc = Rexle.new(File.open('dir.xml','r').read)   
+    a_dir = doc.xpath('records/file/name/text()').sort
 
     return [doc, "nothing new"] if a == a_dir
 
@@ -85,14 +99,11 @@ class DirToXML
     # files to delete
     files_to_delete = a_dir - a
     files_to_delete.each do |filename|
-      node = doc.root.elements["records/file[name='#{filename}']"]
-      node.parent.delete node
+      node = doc.element("records/file[name='#{filename}']")
+      node.delete
     end
 
-    #File.open('dir.xml','w'){|f| f.write pretty_xml(doc)}
     File.open('dir.xml','w'){|f| doc.write f}
     [doc, "updated"]
   end
 end
-
-
